2d304f477e69010b67d6d294d825afa9251fdfea
tristanmryan <151591295+tristanmryan@users.noreply.github.com>
2025-06-27 12:54:49 -0400

    Filter deleted snapshots

--- src/__tests__/HistoricalManager.test.tsx
+++ src/__tests__/HistoricalManager.test.tsx
@@ -2,6 +2,7 @@ import 'fake-indexeddb/auto'
 // polyfill for jest environment
 global.structuredClone =
   global.structuredClone || ((v: any) => JSON.parse(JSON.stringify(v)))
+;
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import React from 'react'
@@ -29,6 +30,20 @@ describe('HistoricalManager', () => {
         <HistoricalManager />
       </SnapshotProvider>
     )
+    expect(await screen.findByText('2024-01')).toBeInTheDocument()
+  })
+
+  test('filters deleted snapshots', async () => {
+    const { softDeleteSnapshot, addSnapshot } = await import('../services/snapshotStore')
+    const altSnap = { ...baseSnap, checksum: 'y' }
+    await addSnapshot(altSnap as any, '2024-02')
+    await softDeleteSnapshot('2024-02')
+    render(
+      <SnapshotProvider>
+        <HistoricalManager />
+      </SnapshotProvider>
+    )
+    expect(screen.queryByText('2024-02')).not.toBeInTheDocument()
     expect(await screen.findByText('2024-01')).toBeInTheDocument()
   })
 })
