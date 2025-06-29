020c793b6ee96b0a6c3b4ac8bfb8d48c28d64fac
tristanmryan <151591295+tristanmryan@users.noreply.github.com>
2025-06-29 12:30:39 -0400

    Fix crypto polyfill and update snapshot

--- src/utils/__tests__/hash.test.ts
+++ src/utils/__tests__/hash.test.ts
@@ -1,8 +1,10 @@
 import { sha1Hex } from '../hash'
 import { TextEncoder } from 'util'
+import { webcrypto as nodeCrypto } from 'crypto'
 
 // polyfill TextEncoder for jsdom environment
-(global as any).TextEncoder = (global as any).TextEncoder || TextEncoder
+(global as any).TextEncoder = (global as any).TextEncoder || TextEncoder;
+(global as any).crypto = (global as any).crypto || nodeCrypto;
 
 describe('sha1Hex', () => {
   it('computes SHA-1 hex for Uint8Array', async () => {
