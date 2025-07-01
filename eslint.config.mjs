9776bdeb08809b03bd6a18b4629ec56e7a7f5a0b
tristanmryan <151591295+tristanmryan@users.noreply.github.com>
2025-07-01 15:37:36 -0400

    feat(build): integrate Tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
       globals: {
         ...globals.browser,
         ...globals.es2020,
@@ -15,6 +19,13 @@ export default [
         ...globals.jest,
       },
     },
+    plugins: { react },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-unused-vars': 'off',
+      'no-useless-catch': 'off',
+    },
   },
-  pluginReact.configs.flat.recommended,
-];
+]
